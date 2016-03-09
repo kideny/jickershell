ca392499ac4c22b4b2cc02a1c62c26518ac956d1
jicker <528803867@qq.com>
2016-03-09 15:14:57 +0800

    完善脚本

--- install.sh
+++ install.sh
@@ -1,2 +1,21 @@
+#安装Tengine的依赖库
 apt-get install pcre-devel openssl openssl-devel
-apt-get install tengine
+
+#进入Debian的源文件目录
+cd /usr/local/src
+
+#下载指定版本的Tengine
+wget http://tengine.taobao.org/download/tengine-2.1.2.tar.gz
+
+#解压缩
+tar zxvf tengine-2.1.2.tar.gz
+
+#进入Tengine的目录
+cd tengine-2.1.2
+
+#配置并检查依赖
+./configure
+
+#编译并且执行安装
+make & make install
+
