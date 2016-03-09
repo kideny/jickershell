1235fb7a23dbac83a05cbe893afea009c0762216
jicker <528803867@qq.com>
2016-03-09 17:28:54 +0800

    添加移出apache2的代码

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ apt-get upgrade
 #安装Tengine的依赖库
 apt-get install pcre-devel openssl openssl-devel gcc-c++ libtool libssl-dev libperl-dev
 
+#移出debian自带的apache2
+apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+
+#杀死所有apache2的进程
+killall apache2
+
 #进入Debian的源文件目录
 cd /usr/local/src
 
