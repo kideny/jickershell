6d3755877de3157823a9df40d813c40b6501e64d
jicker <528803867@qq.com>
2016-03-09 16:02:06 +0800

    添加Tengine的module

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ tar zxvf tengine-2.1.2.tar.gz
 cd tengine-2.1.2
 
 #配置并检查依赖
-./configure --prefix=/usr/local/nginx --group=www-data --user=www-data
+./configure --prefix=/usr/local/nginx --group=www-data --user=www-data  --with-http_stub_status_module --with-http_ssl_module
 
 #编译并且执行安装
 make & make install
