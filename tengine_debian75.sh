94c1db8cb9d8c86c5e59fbf829a6c8d2f6b61e10
jicker <528803867@qq.com>
2016-03-11 09:42:29 +0800

    删除软件包的备份

--- tengine_debian75.sh
+++ tengine_debian75.sh
@@ -16,6 +16,9 @@ apt-get install openssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev
 #移出debian自带的apache2
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
 
+#删除安装软件的备份，释放硬盘空间
+apt-get clean
+
 #杀死所有apache2的进程
 killall apache2
 
