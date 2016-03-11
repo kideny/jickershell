bfbb4965437e7c590d8af3012314819aef733e2b
jicker <528803867@qq.com>
2016-03-11 09:52:57 +0800

    优化安装语句

--- tengine_debian75.sh
+++ tengine_debian75.sh
@@ -11,7 +11,7 @@ apt-get update -y
 apt-get upgrade -y
 
 #安装Tengine的依赖库
-apt-get install openssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev
+apt-get install openssl libssl-dev libtool libperl-dev libpcre3 libpcre3-dev
 
 #移出debian自带的apache2
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
