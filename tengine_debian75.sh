4bbeac310f8ac22a90d4fa3649518d739e955c2f
jicker <528803867@qq.com>
2016-03-11 13:21:27 +0800

    重新提交

--- tengine_debian75.sh
+++ tengine_debian75.sh
@@ -11,16 +11,16 @@ apt-get update -y
 apt-get upgrade -y
 
 #安装Tengine的依赖库
-apt-get install openssl libssl-dev libtool libperl-dev libpcre3 libpcre3-dev
+apt-get install openssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev
 
 #移出debian自带的apache2
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
 
-#删除安装软件的备份，释放硬盘空间
-apt-get clean
-
 #杀死所有apache2的进程
 killall apache2
+
+#删除安装软件的备份，释放硬盘空间
+apt-get clean
 
 #进入Debian的源文件目录
 cd /usr/local/src
@@ -62,7 +62,7 @@ update-rc.d -f nginx defaults
 mkdir -p /usr/local/nginx/conf/vhost
 
 #重新启动Tengine
-service nginx restart
+service nginx start
 
 #安装成功的欢迎致辞！
 echo "Tengine install chenggong!";
