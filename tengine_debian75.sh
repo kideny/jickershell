fbc071c317dcdd3961f6cdb8a340272db93e8a90
jicker <528803867@qq.com>
2016-03-11 15:02:15 +0800

    增加执行脚本的shell的路径

--- tengine_debian75.sh
+++ tengine_debian75.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
 # 检测是否是root账户权限
 if [ $(id -u) != "0" ]; then
     echo "Error: You must be root to run this script, please use root to install ltnmp"
