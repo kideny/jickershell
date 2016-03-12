c56d799bb51501216afd12356995893b826b1299
kideny <528803867@qq.com>
2016-03-13 01:32:16 +0800

    安装php7的指令

--- php7_debian75_product.sh
+++ php7_debian75_product.sh
@@ -21,7 +21,7 @@ apt-get update -y
 apt-get upgrade -y
 
 #安装PHP7的依赖库
-apt-get install
+apt-get install php7 php7-gd
 
 #移出debian自带的apache2及PHP5
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common
