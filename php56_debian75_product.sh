b35d582f19b46a62d1216095d30f5044ac6dbf48
jicker <528803867@qq.com>
2016-03-14 13:09:54 +0800

    增加php编译配置

--- php56_debian75_product.sh
+++ php56_debian75_product.sh
@@ -33,7 +33,7 @@ tar zxvf php-5.6.19.tar.gz
 cd /usr/local/src/php-5.6.19
 
 #配置并检查依赖
-./configure --prefix=/usr/local/php56  --with-config-file-path=/usr/local/php56/etc --with-fpm-user=www-data --with-fpm-group=www-data --with-gd --with-freetype --with-jpeg --with-mcrypt --with-mhash --with-openssl --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --with-curl --with-iconv --with-zlib  --with-gettext --enable-inline-optimization --enable-mbstring --enable-sockets --enable-fpm --enable-opcache --enable-gd-native-ttf --disable-debug --disable-ipv6
+./configure --prefix=/usr/local/php56  --with-config-file-path=/usr/local/php56/etc --with-fpm-user=www-data --with-fpm-group=www-data --with-gd --with-freetype --with-jpeg  --with-mcrypt --with-mhash --with-openssl --with-pdo-mysql=mysqlnd --with-mysqli=mysqlnd --with-mysql=mysqlnd --with-curl --with-iconv --with-zlib  --with-gettext --enable-inline-optimization --enable-ftp --enable-mbstring --enable-sockets --enable-xml --enable-fpm --enable-opcache  --enable-bcmath --enable-gd-native-ttf --enable-soap --enable-zip --disable-debug --disable-ipv6 --disable-rpath --disable-fileinfo
 
 #编译并且执行安装
 time make
