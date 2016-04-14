40eaff22b6c6d3a4a34694141d9cc3ab75bda86f
jicker <528803867@qq.com>
2016-04-14 15:09:08 +0800

    修改install命令

--- tengine/tengine_debian75_product.sh
+++ tengine/tengine_debian75_product.sh
@@ -14,9 +14,6 @@ rm -rf /etc/localtime
 #设置时区为上海
 ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-#升级exim4邮件发送程序
-apt-get install exim4 htop
-
 #对Debian系统Update
 apt-get update -y
 
@@ -24,7 +21,7 @@ apt-get update -y
 apt-get -u upgrade -y
 
 #安装Tengine的依赖库
-apt-get install openssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev
+apt-get install openssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev exim4 htop
 
 #移出debian自带的apache2
 apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
