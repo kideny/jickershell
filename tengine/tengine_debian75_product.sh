752359c47b37494174349e39f94d83c8dda20493
jicker <kideny@aliyun.com>
2016-06-13 16:31:20 +0800

    先执行卸载命令，再执行升级命令

--- tengine/tengine_debian75_product.sh
+++ tengine/tengine_debian75_product.sh
@@ -14,8 +14,11 @@ rm -rf /etc/localtime
 #设置时区为上海
 ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-#先卸载exim4
-apt-get remove -y exim4
+#先卸载exim4及系统自带的apache2
+apt-get remove -y exim4 apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+
+#杀死所有apache2的进程
+killall apache2
 
 #对Debian系统Update
 apt-get update -y
@@ -27,12 +30,6 @@ apt-get -u upgrade -y
 
 #安装Tengine的依赖库
 apt-get install openssl openssl-dev libssl libtool libssl-dev libperl-dev libpcre3 libpcre3-dev htop
-
-#移出debian自带的apache2
-apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
-
-#杀死所有apache2的进程
-killall apache2
 
 #删除安装软件的备份，释放硬盘空间
 apt-get clean
