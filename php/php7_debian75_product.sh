c6456156d69a7979199ed9052fb89b720729c182
jicker <kideny@aliyun.com>
2016-06-21 13:39:05 +0800

    增加默认安装的版本号

--- php/php7_debian75_product.sh
+++ php/php7_debian75_product.sh
@@ -2,6 +2,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+#定义默认安装的php版本号
+defaultversion="7.0.7"
+
 # 检测是否是root账户权限
 if [ $(id -u) != "0" ]; then
     echo "Error: You must be root to run this script, please use root to install ltnmp"
