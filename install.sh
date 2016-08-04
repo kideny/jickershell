ed9b779420ff9b292d4f03e5aa4ae97fc80e074f
jicker <kideny@aliyun.com>
2016-08-04 15:50:41 +0800

    增加环境初始化

--- install.sh
+++ install.sh
@@ -2,3 +2,20 @@
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+#检测是否root账户权限
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install LTNMP"
+    exit 1
+fi
+
+#判定是否开启selinux，如果开启则关闭
+if [ -s /etc/selinux/config ]; then
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+fi
+
+#删除系统自带的时区文件
+rm -rf /etc/localtime
+
+#设置时区为上海
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
