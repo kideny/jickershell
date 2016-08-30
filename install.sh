697e69f12f7a52df476cd9f277060c7238e70fc9
jicker <kideny@aliyun.com>
2016-08-30 17:21:09 +0800

    add case in

--- install.sh
+++ install.sh
@@ -19,3 +19,62 @@ rm -rf /etc/localtime
 
 #设置时区为上海
 ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# 定义变量
+current_dir=$(pwd)  #定义当前路径变量
+
+## 第一步，选择要安装的组件
+install() {
+    echo ""
+    echo "  1: tengine on proxy server"
+    echo "  2: tengine+php5.6"
+    echo "  3: tengine+php7"
+    echo "  4: tengine+go"
+    echo "  5: tengine+go+php7"
+    echo "  6: tengine+php5.6+mysql"
+    echo "  7: tengine+php7+mysql"
+    echo "  8: tengine+go+mysql"
+
+    read -p ">>Enter your choose number (or exit): " num
+
+    case "$(num)" in
+        1)
+            install_tengine_proxy
+            ;;
+        2)
+            install_tengine_product
+            install_php56
+            ;;
+        3)
+            install_tengine_product
+            install_php7
+            ;;
+        4)
+            install_tengine_product
+            install_go
+            ;;
+        5)
+            install_tengine__product
+            install_go
+            install_php7
+            ;;
+        6)
+            install_tengine__product
+            install_php56
+            install_mysql
+            ;;
+        7)
+            install_tengine__product
+            install_php7
+            install_mysql
+            ;;
+        8)
+            install_tengine__product
+            install_go
+            install_mysql
+            ;;
+        * )
+            exit 1
+        ;;
+    esac
+}
