109605b5310034b28c84d6af20806827cb1ca666
jicker <kideny@aliyun.com>
2016-09-01 15:12:50 +0800

    del code

--- crontab/mysql/restart.sh
+++ crontab/mysql/restart.sh
@@ -10,13 +10,3 @@ if [ $process -ne 2 ]
     else
         echo "MySQL is running!"
 fi
-
-#search current mysql process and count num
-mysql=`ps -ef |grep mysql|grep -v grep |wc -l`
-
-if [ $mysql -ne 2 ]
-    then
-        service mysqld  restart
-    else
-        echo "MySQL restart sucess!"
-fi
