5dd14bd2b6ef416ac574048da6b1b19addb4cbee
jicker <kideny@aliyun.com>
2016-09-01 15:04:10 +0800

    add mysql restart.sh

---
+++ crontab/mysql/restart.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# search mysql process and count num
+process=`ps -ef |grep mysql|grep -v grep |wc -l`
+
+#
+if [ $(process) -ne 2 ]
+    then
+        service mysqld  restart
+    else
+        echo "MySQL is running!"
+fi
+
+#search current mysql process and count num
+mysql=`ps -ef |grep mysql|grep -v grep |wc -l`
+
+if [ $(mysql) -ne 2 ]
+    then
+        service mysqld  restart
+    else
+        echo "MySQL restart sucess!"
+fi
