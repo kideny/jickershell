aa5e72c2d81099665adef06f3b9a216c80d4d52c
jicker <kideny@aliyun.com>
2016-09-26 16:48:35 +0800

    dev

---
+++ debian/git/webhooks/GitAction.class.php
@@ -0,0 +1,11 @@
+<?php
+// 本类主要用于跟git交互的webhook
+class GitAction extends HCommonAction {
+
+	//拉取接口
+    public function pull(){
+        //进入网站目录，执行拉取master的命令
+        shell_exec("cd  /web/www_test_com/default ; git pull origin master 2>&1 | tee -a /tmp/mylog 2>/dev/null >/dev/null &");
+
+    }
+}
