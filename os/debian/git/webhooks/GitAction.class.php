de657a9c58c6915e877a4c0fdb8e3455042f6da0
Frank Yuan <kideny@msn.com>
2016-11-29 16:23:21 +0800

    test

--- os/debian/git/webhooks/GitAction.class.php
+++ os/debian/git/webhooks/GitAction.class.php
@@ -7,6 +7,7 @@ class GitAction extends HCommonAction {
     public function pull(){
         //进入网站目录，执行拉取master的命令
         shell_exec("cd  /web/www_test_com/default ; git pull origin master 2>&1 | tee -a /tmp/mylog 2>/dev/null >/dev/null &");
+        echo "天佑中华是毛片！";
 
     }
 }
