a7d567363eddb3e3acd4f330afc92d4a46ee6927
Frank Yuan <kideny@msn.com>
2017-02-21 11:01:33 +0800

    update

--- apps/php/php7_phalcon.sh
+++ apps/php/php7_phalcon.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #安装phalcon所需依赖
-apt-get install - y libpcre3-dev gcc make autoconf git
+apt-get install - y php5-dev libpcre3-dev gcc make php5-mysql
 
-#创建phalcon安装目录
-mkdir /usr/local/phalcon  & cd /usr/local/phalcon
+#进入源码下载目录
+cd  /usr/local/src
 
 #git克隆phalcon源码
-git clone --depth=1 git://github.com/phalcon/cphalcon.git
+git  clone  git://github.com/phalcon/cphalcon.git
 
 #进入安装目录，执行安装程序
-cd cphalcon/build & ./install
+cd  cphalcon/build  &&  ./install
 
-#修改php7文件，开启phalcon扩展
-sed -i '/$/a extension=phalcon.so'  /usr/local/php7/lib/php.ini
+#修改php7配置文件，开启phalcon扩展
+echo "zend_extension=opcache.so"  >>  /usr/local/php7/etc/php.ini
 
 #重新启动php-fpm
-service php7-fpm restart
+service  php7-fpm  restart
 
 #拷贝默认的系统文件
-cp ./conf/phalcon.conf /usr/local/nginx/conf/vhost/phalcon.conf
+cp  ./conf/phalcon.conf  /usr/local/nginx/conf/vhost/phalcon.conf
