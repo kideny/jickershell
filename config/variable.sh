8bcb05135afb7ccdeba9cae8887aaf0a15c02fb9
kideny <kideny@gmail.com>
2019-10-26 12:27:39 +0800

    fixed

--- config/variable.sh
+++ config/variable.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Nginx Version  https://nginx.org/en/download.html
-# Openresty Version  https://openresty.org/cn/download.html
 # Docker Version  https://www.docker.com/community-edition#/download
 # Docker Debian   https://docs.docker.com/install/linux/docker-ce/debian/
 # Docker Ubuntu   https://docs.docker.com/install/linux/docker-ce/ubuntu/
@@ -12,13 +10,13 @@ current_dir=$(pwd)
 # 定义默认安装程序的下载路径
 srcDir="/usr/local/src"
 
-# 定义默认的Phalcon的版本  
+# Phalcon Version https://github.com/phalcon/cphalcon/releases
 defualtPhalconVersion="3.4.4"
 
-# PHP Version    http://www.php.net/
-defaultPHP70Version="7.3.3"
-defaultPHP72Version="7.2.21"
-defaultPHP71Version="7.1.31"
+# PHP Version http://www.php.net/
+defaultPHP70Version="7.3.11"
+defaultPHP72Version="7.2.24"
+defaultPHP71Version="7.1.33"
 
 # 定义默认的Nginx的版本 http://nginx.org/en/download.html
 defaultNginxVersion="1.16.0"
